a50ba657a5ec261f35a79cfbe888cc156ec552d1
Anton-Latukha <anton.latukha@gmail.com>
2020-10-16 01:32:39 +0300

    M  run-action.sh

--- run-action.sh
+++ run-action.sh
@@ -6,3 +6,5 @@ cabal2nix . > project-derivation.nix
 curl -L "https://github.com/NixOS/nixpkgs/archive/$rev.tar.gz" | tar -xz
 cd "nixpkgs-$rev" || exit 1
 
+integrationPointFile='pkgs/development/haskell-modules/non-hackage-packages.nix'
+
