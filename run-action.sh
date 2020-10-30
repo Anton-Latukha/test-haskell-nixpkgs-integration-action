d170155908cc9d9fe45f60a8dccc766847809825
Anton-Latukha <anton.latukha@gmail.com>
2020-10-31 01:11:07 +0200

    M  run-action.sh: refactor some more

--- run-action.sh
+++ run-action.sh
@@ -1,29 +1,30 @@
 #!/usr/bin/env bash
 
-rev="${rev:-master}"
+rev=${rev:-master}
 
-projectDir="$(pwd)"
-cabal2nix . > project-derivation.nix
-derivationName='integratedDerivation'
+projectDir=$(pwd)
+derivationName=integratedDerivation
+projectDerivationFile=project-derivation.nix
+cabal2nix . > "$projectDerivationFile"
 cd ..
 # IDK why, but particularly inside CI Tar complains the stdin not being the tar archive, but then unarchives it, who would have thought.
 curl -L "https://github.com/NixOS/nixpkgs/archive/$rev.tar.gz" | tar -xz 2> /dev/null
-nixpkgsDir="nixpkgs-$rev"
+nixpkgsDir=nixpkgs-$rev
 cd "$nixpkgsDir" || exit 1
 
-integrationPointFile='pkgs/development/haskell-modules/non-hackage-packages.nix'
+integrationPointFile=pkgs/development/haskell-modules/non-hackage-packages.nix
 
 # Remove black lines from the end of the file
 sed -i -e :a -e '/^\n*$/{$d;N;ba' -e '}' "$integrationPointFile"
 
 # Store the number of lines in the file
-lineNumToInsertAt="$(wc -l "$integrationPointFile" | cut -f1 -d' ')"
+lineNumToInsertAt=$(wc -l "$integrationPointFile" | cut -f1 -d' ')
 
-lineToInsert='  $derivationName = self.callPackage '"$projectDir"'/project-derivation.nix {};'
+lineToInsert=" $derivationName = self.callPackage $projectDir/$projectDerivationFile {};"
 # Modify the file
 sed -i "$lineNumToInsertAt"'i'"$lineToInsert" "$integrationPointFile"
 
-cat "$projectDir/project-derivation.nix"
+cat "$projectDir/$projectDerivationFile"
 cat "$integrationPointFile"
 
-nix-build . -A 'haskellPackages.'"$derivationName"
+nix-build . -A "haskellPackages.$derivationName"
