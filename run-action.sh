0a85acb983c7fdcf8453fea482f3361cc7d66fb3
Anton-Latukha <anton.latukha@gmail.com>
2020-10-31 01:31:24 +0200

    M  run-action.sh: more refactoring

--- run-action.sh
+++ run-action.sh
@@ -3,12 +3,13 @@
 rev=${rev:-master}
 
 projectDir=$(pwd)
+projectDirName=$(basename "$projectDir")
 derivationName=integratedDerivation
 projectDerivationFile=project-derivation.nix
-cabal2nix . > "$projectDerivationFile"
 cd ..
+cabal2nix ./"$projectDir" > "$projectDerivationFile"
 # IDK why, but particularly inside CI Tar complains the stdin not being the tar archive, but then unarchives it, who would have thought.
-curl -L "https://github.com/NixOS/nixpkgs/archive/$rev.tar.gz" | tar -xz 2> /dev/null
+curl -L "https://github.com/NixOS/nixpkgs/archive/$rev.tar.gz" | tar -xz
 nixpkgsDir=nixpkgs-$rev
 cd "$nixpkgsDir" || exit 1
 
@@ -20,7 +21,7 @@ sed -i -e :a -e '/^\n*$/{$d;N;ba' -e '}' "$integrationPointFile"
 # Store the number of lines in the file
 lineNumToInsertAt=$(wc -l "$integrationPointFile" | cut -f1 -d' ')
 
-lineToInsert=" $derivationName = self.callPackage $projectDir/$projectDerivationFile {};"
+lineToInsert=" $derivationName = self.callPackage ../../../$projectDerivationFile {};"
 # Modify the file
 sed -i "$lineNumToInsertAt"'i'"$lineToInsert" "$integrationPointFile"
 
