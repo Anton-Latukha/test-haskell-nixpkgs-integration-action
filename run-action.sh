0958154e3be8bc1f599118a9045430d0a44e4b8d
Anton-Latukha <anton.latukha@gmail.com>
2020-10-31 21:34:05 +0200

    M  run-action.sh: fxs

--- run-action.sh
+++ run-action.sh
@@ -3,12 +3,12 @@
 rev=${rev:-master}
 
 projectDir=$(pwd)
+echo "Project directory: $projectDir"
 projectDirName=$(basename "$projectDir")
 derivationName=integratedDerivation
 projectDerivationFile=project-derivation.nix
-echo "Project PWD: $(pwd)"
 cd ..
-echo "Now PWD is: $(pwd)"
+echo "Now directory is: $(pwd)"
 cabal2nix ./"$projectDir" > "$projectDerivationFile"
 # IDK why, but particularly inside CI Tar complains the stdin not being the tar archive, but then unarchives it, who would have thought.
 curl -L "https://github.com/NixOS/nixpkgs/archive/$rev.tar.gz" | tar -xz
@@ -23,7 +23,7 @@ sed -i -e :a -e '/^\n*$/{$d;N;ba' -e '}' "$integrationPointFile"
 # Store the number of lines in the file
 lineNumToInsertAt=$(wc -l "$integrationPointFile" | cut -f1 -d' ')
 
-lineToInsert=" $derivationName = self.callPackage ../../../$projectDerivationFile {};"
+lineToInsert=" $derivationName = self.callPackage ../../../$projectDirName/$projectDerivationFile {};"
 # Modify the file
 sed -i "$lineNumToInsertAt"'i'"$lineToInsert" "$integrationPointFile"
 
