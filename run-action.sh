2f1c47de19b12d176564bd82c110b67de74e41f7
Anton-Latukha <anton.latukha@gmail.com>
2020-11-01 12:54:57 +0200

    M  run-action.sh: clean-up

--- run-action.sh
+++ run-action.sh
@@ -10,26 +10,15 @@ echo "Project directory: $projectDir"
 projectDirName=$(basename "$projectDir")
 derivationName=integratedDerivation
 projectDerivationFile=project-derivation.nix
-cabal2nix .
+
 cabal2nix . > "$projectDerivationFile"
 
-ls
-
-cat "$projectDerivationFile"
-
 cd ..
-echo "Now directory is ground dir: $(pwd)"
-
-ls
-
 
 # IDK why, but particularly inside CI Tar complains the stdin not being the tar archive, but then unarchives it, who would have thought.
 curl -L "https://github.com/NixOS/nixpkgs/archive/$rev.tar.gz" | tar -xz
 nixpkgsDir=nixpkgs-$rev
 cd "$nixpkgsDir" || exit 1
-echo "Now directory is Nixpkgs dir: $(pwd)"
-
-ls
 
 integrationPointFile=pkgs/development/haskell-modules/non-hackage-packages.nix
 
@@ -42,12 +31,10 @@ rm tmpFile
 lineNumToInsertAt=$(wc -l "$integrationPointFile" | cut -f1 -d' ')
 
 lineToInsert=" $derivationName = self.callPackage ../../../../$projectDirName/$projectDerivationFile {};"
+
 # Modify the file
 cp "$integrationPointFile" tmpFile
 sed "$lineNumToInsertAt"'i'"$lineToInsert" tmpFile > "$integrationPointFile"
 rm tmpFile
-cat "$integrationPointFile"
-
-echo "Checking derivation file: $(type "$projectDir/$projectDerivationFile")"
 
 nix-build . -A "haskellPackages.$derivationName"
